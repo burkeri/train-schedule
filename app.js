e0403ca79aa3ef19b992795db9ace713e82a4f67
Richelle Burke <richelleburke@Richelles-MacBook-Pro.local>
2018-11-28 17:31:31 -0500

    html/css finished --> begin js

---
+++ app.js
@@ -0,0 +1,9 @@
+// when form is submitted,
+// 1) save the data to a variable
+// 2) push info to firebase
+// 3) calculate train next arrival 
+// 4) calculate train minutes away
+// 4) create new table data
+// 5) create new table row
+// 6) push the data into the row
+// 7) append the row to the div
